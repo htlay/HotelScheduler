e9fe377d71efa673ac09835182bab505e4a7ff41
htlay <hunglay0420@gmail.com>
2016-08-17 20:09:03 -0700

    adding menu

--- WebApplication1/About.aspx.cs
+++ WebApplication1/About.aspx.cs
@@ -7,14 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication1
 {
-    public partial class Site : System.Web.UI.MasterPage
+    public partial class About : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
         {
 
         }
